71d2afa3b3417e389507796f34e2e4cd528f57ea
Taufik-THD <ovick_kd@yahoo.com>
2021-03-10 15:23:26 +0700

    add disabler button at internal testing

--- .nuxt/dist/server/pages/index.js
+++ .nuxt/dist/server/pages/index.js
@@ -2324,12 +2324,12 @@ const dirtyTypes = ['color', 'file', 'time', 'date', 'datetime-local', 'week', '
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=01937e8b&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"pa-3 pt-12",staticStyle:{"overflow-y":"scroll !important","max-height":"100vh"}},[_c('center',{staticClass:"mb-8 mt-3"},[_c('v-img',{attrs:{"src":"https://faq.balesin.id/icon.ico","width":"210"}}),_vm._v(" "),_c('div',{staticClass:"mt-6",staticStyle:{"font-weight":"600"}},[_vm._v("\n      balesin shop - Internal testing\n    ")])],1),_vm._ssrNode(" "),_c('v-card',{attrs:{"flat":""}},[_c('v-text-field',{attrs:{"label":"Phone","placeholder":"628123456789","outlined":""},model:{value:(_vm.uuid),callback:function ($$v) {_vm.uuid=$$v},expression:"uuid"}})],1),_vm._ssrNode(" "),_c('v-card',{attrs:{"flat":""}},[_c('v-text-field',{attrs:{"label":"Bot Name","placeholder":"sarirotiwa","outlined":""},model:{value:(_vm.bot_name),callback:function ($$v) {_vm.bot_name=$$v},expression:"bot_name"}})],1),_vm._ssrNode(" "),_c('v-btn',{attrs:{"color":"#FD0","block":""},on:{"click":_vm.find_user}},[_vm._v("shop")]),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"mt-4\">","</div>",[_c('v-btn',{attrs:{"color":"#FD0","block":""},on:{"click":_vm.reg_user}},[_vm._v("register")])],1),_vm._ssrNode(" "),_c('v-card',{attrs:{"height":"5vh","flat":""}})],2)}
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=56c01ace&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"pa-3 pt-12",staticStyle:{"overflow-y":"scroll !important","max-height":"100vh"}},[_c('center',{staticClass:"mb-8 mt-3"},[_c('v-img',{attrs:{"src":"https://faq.balesin.id/icon.ico","width":"210"}}),_vm._v(" "),_c('div',{staticClass:"mt-6",staticStyle:{"font-weight":"600"}},[_vm._v("\n      balesin shop - Internal testing\n    ")])],1),_vm._ssrNode(" "),_c('v-card',{attrs:{"flat":""}},[_c('v-text-field',{attrs:{"label":"Phone","placeholder":"628123456789","outlined":""},model:{value:(_vm.uuid),callback:function ($$v) {_vm.uuid=$$v},expression:"uuid"}})],1),_vm._ssrNode(" "),_c('v-card',{attrs:{"flat":""}},[_c('v-text-field',{attrs:{"label":"Bot Name","placeholder":"sarirotiwa","outlined":""},model:{value:(_vm.bot_name),callback:function ($$v) {_vm.bot_name=$$v},expression:"bot_name"}})],1),_vm._ssrNode(" "),_c('v-btn',{attrs:{"disabled":_vm.active_btn,"color":"#FD0","block":""},on:{"click":_vm.find_user}},[_vm._v("shop")]),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"mt-4\">","</div>",[_c('v-btn',{attrs:{"disabled":_vm.active_btn,"color":"#FD0","block":""},on:{"click":_vm.reg_user}},[_vm._v("register")])],1),_vm._ssrNode(" "),_c('v-card',{attrs:{"height":"5vh","flat":""}})],2)}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=01937e8b&
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=56c01ace&
 
 // CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
 //
@@ -2360,6 +2360,20 @@ var staticRenderFns = []
     uuid: '',
     bot_name: 'sarirotiwa'
   }),
+  watch: {
+    uuid(newval, oldval) {
+      if (newval[0] == '0') {
+        this.uuid = `62${newval.slice(1)}`;
+      }
+    }
+
+  },
+  computed: {
+    active_btn() {
+      return this.uuid.length == 0;
+    }
+
+  },
   methods: {
     async find_user() {
       const {
--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,9 @@
     <v-card flat>
       <v-text-field label="Bot Name" placeholder="sarirotiwa" v-model="bot_name" outlined />
     </v-card>
-    <v-btn color="#FD0" block @click="find_user">shop</v-btn>
+    <v-btn :disabled="active_btn" color="#FD0" block @click="find_user">shop</v-btn>
     <div class="mt-4">
-      <v-btn color="#FD0" block @click="reg_user">register</v-btn>
+      <v-btn :disabled="active_btn" color="#FD0" block @click="reg_user">register</v-btn>
     </div>
     <v-card height="5vh" flat />
   </div>
@@ -26,8 +26,22 @@ export default {
 
   data: () => ({
     uuid: '',
-    bot_name: 'sarirotiwa'
+    bot_name: 'sarirotiwa',
   }),
+
+  watch: {
+    uuid(newval, oldval) {
+      if (newval[0] == '0') {
+        this.uuid = `62${newval.slice(1)}`
+      }
+    }
+  },
+
+  computed: {
+    active_btn () {
+      return this.uuid.length == 0
+    }
+  },
 
   methods: {
     async find_user() {
