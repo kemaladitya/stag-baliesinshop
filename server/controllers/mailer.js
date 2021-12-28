e4c96172176697ccfc6fe483cc9c454bc0144170
Taufik-THD <ovick_kd@yahoo.com>
2021-12-28 16:24:14 +0700

    turn off mail logger

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -5,17 +5,20 @@ const { mailer_list } = require('../../config.json')
 
 async function send(request, response) {
   try {
-    axios({
-      url: "http://10.140.0.4:3333/mailer/send",
-      method: "post",
-      data: {
-        receiver : mailer_list,
-        sender   : 'shop@balesin.id',
-        subject  : 'customer request failed.',
-        text     : 'customer request failed.',
-        template : request.body.message,
-      },
-    });
+    // axios({
+    //   url: "http://10.140.0.4:3333/mailer/send",
+    //   method: "post",
+    //   data: {
+    //     receiver : mailer_list,
+    //     sender   : 'shop@balesin.id',
+    //     subject  : 'customer request failed.',
+    //     text     : 'customer request failed.',
+    //     template : request.body.message,
+    //   },
+    // });
+    console.log("@error-message-from-frontend: ", request.body.message);
+
+    return response.send("OK")
   } catch (error) {
     console.log(error)
 
