b6b8f8cf908d0b6c7a09ac6a3190550499101dad
Taufik-THD <ovick_kd@yahoo.com>
2021-12-29 13:22:18 +0700

    turn off axios request timeout

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,17 +10,12 @@ const callback = require("./callback");
 const axios   = require('axios')
 const { _api, _key } = require('../../config.json')
 
-let service = 0;
-
 async function general(request, response) {
   try {
-    service = !service;
-
     const { data } = await axios({
-      url     : (service ? _api : "http://10.140.0.14:3098") + "/dev" + request.url,
+      url     : _api + "/dev" + request.url,
       method  : request.method.toLowerCase(),
       headers : { "x-api-key": _key, ...request.headers },
-      timeout : 5000,
       data    : request.body,
     });
 
@@ -35,31 +30,6 @@ async function general(request, response) {
       )
   } catch (error) {
     console.log(error);
-
-    if (!request.url.includes("order") || !request.url.includes("register")) {
-      try {
-        console.log("--move");
-        const { data } = await axios({
-          url: "http://10.140.0.14:3098" + "/dev" + request.url,
-          method: request.method.toLowerCase(),
-          headers: { "x-api-key": _key, ...request.headers },
-          timeout: 1000,
-          data: request.body,
-        });
-
-        // console.log("-- end", data);
-        // console.log("move request to http://10.140.0.14:3088");
-
-        return response.json(data.hasOwnProperty("data") ? data.data : data);
-      } catch (error) {
-        console.log(error);
-        return response.json({
-          message: String(error),
-          status: false,
-          response: "",
-        });
-      }
-    }
 
     return response.json({
       message: String(error),
